e97e0fd91e3de52f9886ef9a922701a398f49286
akcays <ilkesedefakcay@gmail.com>
2017-05-08 13:12:35 -0700

    Implemented post method to query f2f api and return results

--- recipely-server/server.js
+++ recipely-server/server.js
@@ -1,13 +1,27 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var axios = require('axios');
+
+var key = require('./config/config');
 
 var morgan = require('morgan');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
+app.post('/api/recipes', (req, res) => {
+  var ingredients = req.body.ingredients;
+  axios.get(`http://food2fork.com/api/search?key=${key}&q=${ingredients}`)
+    .then(response => {
+      console.log('Success!', response.data.recipes);
+      res.json(response.data.recipes);
+    })
+    .catch(e => console.error(e));
+  });
+
 
 app.listen(8080, function() {
   console.log('Server is now listening on port 8080');
 });
+
