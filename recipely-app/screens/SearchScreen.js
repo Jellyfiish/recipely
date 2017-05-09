60871f3f07872fd8b3a846db4cc764f896a0c451
John Cheng <chengj@gmail.com>
2017-05-09 16:33:50 -0700

    Render list of items and add button to remove items

--- recipely-app/screens/SearchScreen.js
+++ recipely-app/screens/SearchScreen.js
@@ -3,17 +3,81 @@ import {
   StyleSheet,
   Text,
   View,
-  Button
+  ScrollView,
+  FlatList,
+  Button,
+  TextInput,
+  KeyboardAvoidingView
 } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
 
 class SearchScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ingredients: [
+        {name: 'blueberry'},
+        {name: 'strawberry'},
+        {name: 'blackberry'}
+      ],
+      input: '',
+    };
+  }
+
+  addIngredients = (event) => {
+    this.setState({
+      ingredients: [ {name: event.nativeEvent.text}, ...this.state.ingredients],
+      input: '',
+    });
+  };
+
+  onInputChange = (event) => {
+    this.setState({input: event.nativeEvent.text});
+  };
+
+  onRemovePress = (i) => {
+    const ingredients = this.state.ingredients;
+    this.setState({
+      ingredients: [ ...ingredients.slice(0, i), ...ingredients.slice(i + 1)]
+    });
+  };
+
+  onFindRecipesPress = () => {
+    this.props.navigation.navigate('SearchResults');
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Search recipes</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Add an ingredient"
+          value={this.state.input}
+          onChange={this.onInputChange}
+          onSubmitEditing={this.addIngredients}
+        />
+
+        <Text>Ingredients:</Text>
+
+        <ScrollView>
+          { this.state.ingredients.map((item, i) => {
+              return (
+                <View key={i}>
+                  <Text>{item.name}</Text>
+                  <Button
+                    title="Remove"
+                    onPress={() => this.onRemovePress(i)}
+                  />
+                </View>
+              );
+            })
+          }
+        </ScrollView>
+
         <Button
           title="Find recipes"
-          onPress={() => this.props.navigation.navigate('SearchResults')}
+          onPress={() => this.onFindRecipesPress}
         />
       </View>
     );
@@ -23,8 +87,10 @@ class SearchScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  textInput: {
+    height: 50,
+    padding: 15,
   },
 });
 
