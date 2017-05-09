4b67f9d5b6a854655ac0965006cbc9a1a1e579c6
John Cheng <chengj@gmail.com>
2017-05-08 18:54:20 -0700

    Change layout of photo screen

--- recipely-app/screens/PhotoScreen.js
+++ recipely-app/screens/PhotoScreen.js
@@ -8,6 +8,7 @@ import {
   Button,
   Image
 } from 'react-native';
+import { Icon } from 'react-native-elements'
 
 class PhotoScreen extends Component {
   constructor(props) {
@@ -42,17 +43,34 @@ class PhotoScreen extends Component {
     let { image } = this.props.screenProps;
 
     return (
-      <View style={styles.container}>
-        <Button
-          title="Take a picture with your camera"
-          onPress={this.takeImage}
-        />
-        <Button
-          title="Pick an image from camera roll"
-          onPress={this.pickImage}
-        />
-      { image &&
-          <Image source={{ uri: image.uri }} style={styles.image} />}
+      <View style={{flex: 1}}>
+
+        <View style={styles.imageContainer}>
+          { image &&
+            <Image source={{ uri: image.uri }} style={styles.image} />
+          }
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <View style={styles.buttonLabel}>
+            <Icon
+              name="photo-album"
+              size={30}
+              raised
+              onPress={this.pickImage}
+            />
+            <Text>Use album</Text>
+          </View>
+          <View style={styles.buttonLabel}>
+            <Icon
+              name="photo-camera"
+              size={30}
+              raised
+              onPress={this.takeImage}
+            />
+          <Text>Use camera</Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -64,10 +82,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 4,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  buttonLabel: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   image: {
     width: 300,
     height: 300,
-    resizeMode: 'contain'
+    resizeMode: 'contain',
   },
 });
 
