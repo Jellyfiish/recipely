dae2f1274b80f2790edb6232a1ad71714b7241e5
obay <obaymardini@gmail.com>
2017-05-10 02:32:30 +0200

    updated the primary key in the recipes table and reformated the foreign keys accordingly

--- recipely-server/models/database.js
+++ recipely-server/models/database.js
@@ -18,22 +18,23 @@ var dbAuth = dbParams.auth.split(':');
 
 var config = {
   user: dbAuth[0],
-  password: dbAuth[1],
+  password: '12345',
   host: dbParams.hostname,
   port: dbParams.port,
   database: dbParams.pathname.split('/')[1],
   max: 20,
   idleTimeoutMillis: 5000
 };
-
+  //"postgres://obay:obay@localhost:5432/recipely"
+console.log(config)
 var client = new pg.Pool(config);
 
 // table creation SQL queries
 var tableQueries = [
   'CREATE TABLE users(ID SERIAL PRIMARY KEY, username VARCHAR(40) not null, password VARCHAR(40) not null)',
-  'CREATE TABLE recipes(ID SERIAL PRIMARY KEY, title VARCHAR(40) not null, ingredients VARCHAR(255) not null, source_url VARCHAR(255) not null, thumbnail_url VARCHAR(255) not null, saved_count INTEGER not null)',
-  'CREATE TABLE recipes_users(ID SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users (id), recipe_id INTEGER REFERENCES recipes (id))',
-  'CREATE TABLE notes(ID SERIAL PRIMARY KEY, text VARCHAR(255) not null, user_id INTEGER REFERENCES users (id) not null, recipe_id INTEGER REFERENCES recipes (id) not null)'
+  'CREATE TABLE recipes(f2f_id varchar(40) PRIMARY KEY, title VARCHAR(40) not null, ingredients VARCHAR(255) not null, source_url VARCHAR(255) not null, thumbnail_url VARCHAR(255) not null, saved_count INTEGER not null)',
+  'CREATE TABLE recipes_users(ID SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users (id), f2f_id VARCHAR(40) REFERENCES recipes (f2f_id))',
+  'CREATE TABLE notes(ID SERIAL PRIMARY KEY, text VARCHAR(255) not null, user_id INTEGER REFERENCES users (id) not null, f2f_id VARCHAR(40) REFERENCES recipes (f2f_id) not null)'
 ];
 
 tableQueries.forEach(function(query) {
