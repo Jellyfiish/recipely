612574c97ebdd4703e1caee4cc8c8635b8a400d5
obay <obaymardini@gmail.com>
2017-05-10 05:04:02 +0200

    modify the password max-length to be 64 chars

--- recipely-server/models/database.js
+++ recipely-server/models/database.js
@@ -31,7 +31,7 @@ var client = new pg.Pool(config);
 
 // table creation SQL queries
 var tableQueries = [
-  'CREATE TABLE users(ID SERIAL PRIMARY KEY, username VARCHAR(40) not null, password VARCHAR(40) not null)',
+  'CREATE TABLE users(ID SERIAL PRIMARY KEY, username VARCHAR(40) not null, password VARCHAR(64) not null)',
   'CREATE TABLE recipes(f2f_id varchar(40) PRIMARY KEY, title VARCHAR(40) not null, ingredients VARCHAR(255) not null, source_url VARCHAR(255) not null, thumbnail_url VARCHAR(255) not null, saved_count INTEGER not null)',
   'CREATE TABLE recipes_users(ID SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users (id), f2f_id VARCHAR(40) REFERENCES recipes (f2f_id))',
   'CREATE TABLE notes(ID SERIAL PRIMARY KEY, text VARCHAR(255) not null, user_id INTEGER REFERENCES users (id) not null, f2f_id VARCHAR(40) REFERENCES recipes (f2f_id) not null)'
