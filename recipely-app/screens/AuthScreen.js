40ea04004647407e67070ce7f15700a7cbc65850
John Cheng <chengj@gmail.com>
2017-05-11 10:26:28 -0700

    Add form to auth screen

--- recipely-app/screens/AuthScreen.js
+++ recipely-app/screens/AuthScreen.js
@@ -1,16 +1,63 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 import { FormLabel, FormInput } from 'react-native-elements';
 
 class AuthScreen extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      username: '',
+      password: '',
+    };
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Auth screen!</Text>
+
+        <View style={styles.nameContainer}>
+          <Text>recipely</Text>
+        </View>
+
+        <View style={styles.wrapper}>
+          <View style={styles.inputWrap}>
+            <TextInput
+              style={styles.input}
+              underlineColorAndroid="transparent"
+              placeholder="Username"
+              value={this.state.username}
+              onChangeText={(username) => this.setState({username})}
+            />
+          </View>
+
+          <View style={styles.inputWrap}>
+            <TextInput
+              style={styles.input}
+              underlineColorAndroid="transparent"
+              placeholder="Password"
+              secureTextEntry={true}
+              value={this.state.password}
+              onChangeText={(password) => this.setState({password})}
+            />
+          </View>
+
+          <View style={styles.button}>
+            <Button
+              title="Login"
+              onPress={() => {}}
+            />
+          </View>
+
+          <View style={styles.button}>
+            <Button
+              title="Signup"
+              onPress={() => {}}
+            />
+          </View>
+        </View>
+
+        <View style={{flex: 3}} />
       </View>
     );
   }
@@ -19,8 +66,28 @@ class AuthScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  nameContainer: {
+    flex: 2,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  wrapper: {
+    paddingHorizontal: 15,
+  },
+  inputWrap: {
+    flexDirection: 'row',
+    marginVertical: 10,
+    height: 40,
+    backgroundColor: 'transparent',
+  },
+  input: {
+    flex: 1,
+    paddingHorizontal: 10,
+    backgroundColor: '#fff',
+  },
+  button: {
+    marginVertical: 5,
   },
 });
 
