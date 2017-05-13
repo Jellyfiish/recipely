e705155f1cdbf1cbc3bba3da1bd122a6e3e26e4b
John Cheng <chengj@gmail.com>
2017-05-12 20:30:26 -0700

    Fetch popular recipes and render in popular screen

--- recipely-app/screens/PopularScreen.js
+++ recipely-app/screens/PopularScreen.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
 import {
+  ActivityIndicator,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+import ResultList from '../components/ResultList';
 
 class PopularScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      recipes: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jellyfiish-recipely.herokuapp.com/api/recipes')
+      .then(res => res.json())
+      .then(result => {
+          this.setState({recipes: result.recipes}, () => console.log('fetched popular recipes!'));
+        }
+      );
+  }
+
+  onSearchChange = (query, recipes) => {
+    this.setState({recipes});
+  };
+
   render() {
+    const {
+      idToken,
+      onRecipesChange,
+      recipes: savedRecipes,
+    } = this.props.screenProps;
+    const { navigation } = this.props;
+
     return (
-      <View style={styles.container}>
-        <Text>Popular recipes</Text>
+      <View>
+
+        { this.state.recipes.length !== 0
+          ? <ResultList
+              navigation={navigation}
+              recipes={this.state.recipes}
+              savedRecipes={savedRecipes}
+              idToken={idToken}
+              onRecipesChange={onRecipesChange}
+              onSearchChange={this.onSearchChange}
+            />
+          : <View>
+              <Text>Loading recipes</Text>
+              <ActivityIndicator size="large" />
+            </View>
+        }
       </View>
     );
   }
