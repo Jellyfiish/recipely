4f71d55f02ca07b1cef0aafd909d63784df5d86d
mschlesk <markschleske@gmail.com>
2017-05-15 17:37:53 -0600

    Update configuration file example. Add descriptive comments.

--- recipely-server/config/config.example.js
+++ recipely-server/config/config.example.js
@@ -1,5 +1,12 @@
 module.exports = {
+  // Food2Fork API key
   F2F_API_KEY: 'F2F_API_KEY',
+  // URL to your local postgres server (e.g. "postgres://user:password@localhost:5432/mydatabase")
   DATABASE_URL: 'DATABASE_URL',
-  TOKEN_SECRET: 'TOKEN_SECRET'
+  // Secret string used to encode the user authentication tokens
+  TOKEN_SECRET: 'TOKEN_SECRET',
+  // Clarifai API client id
+  CLIENT_ID: 'CLIENT_ID',
+  // Clarifai API client secret
+  CLIENT_SECRET: 'CLIENT_SECRET'
 };
