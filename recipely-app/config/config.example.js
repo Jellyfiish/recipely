8f1a65204ffd02c08f0d2f889036e5a7b280de83
John Cheng <chengj@gmail.com>
2017-05-15 19:19:02 -0700

    Change how Clarifai token is retrieved

--- recipely-app/config/config.example.js
+++
@@ -1,9 +0,0 @@
-import base64 from 'base-64';
-
-// clarifai api keys
-const CLIENT_ID = 'CLIENT_ID';
-const CLIENT_SECRET = 'CLIENT_SECRET';
-
-const auth = base64.encode(`${CLIENT_ID}:${CLIENT_SECRET}`);
-
-export default auth;
